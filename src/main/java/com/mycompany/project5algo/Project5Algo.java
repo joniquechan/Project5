ed22e4daaee2c3dff0a9c296390bb62208ba74b5
joniquechan <uniquejoniqueC@gmail.com>
2024-11-20 00:12:56 -0500

    pretty much done except for comments

--- src/main/java/com/mycompany/project5algo/Project5Algo.java
+++ src/main/java/com/mycompany/project5algo/Project5Algo.java
@@ -39,7 +39,7 @@ public class Project5Algo {
                 dist[col][row] = distance;
             }
 
-            // initialize cities 
+            // initialize cities (key-value)
             Map<Integer, String> citiesMap = new HashMap<>();
             for (int j = 0; j < cities; j++) {
                 int node = sc.nextInt();
@@ -69,25 +69,53 @@ public class Project5Algo {
                 }
             }
             
+            /*
             // print 2d array - for debugging
             for (int j = 0; j < dist.length; j++) { 
-                for (int k = 0; k < dist[i].length; k++) { 
+                for (int k = 0; k < dist[j].length; k++) { 
                     System.out.print(dist[j][k] + " "); 
                 } 
                 System.out.println(); 
-            }            
+            }
+                */
             
             // go through each sign and check if shortest path goes through each node
+            int index = 1;
             for (Sign sign : signs) {
+                int start = sign.from;
+                int end = sign.to;
+                double signDist = sign.distance;
+
+                // list of valid signs
+                List<Sign> validSigns = new ArrayList<>();
                 
+                // iterate through each entry in citiesMap
+                for (Map.Entry<Integer, String> entry : citiesMap.entrySet()) {
+                    int cityNode = entry.getKey();
+                    String cityName = entry.getValue();
+                    
+                    // check if shortest path goes through both nodes in signs
+                    if (dist[start][cityNode] == dist[start][end] + dist[end][cityNode]) {
+                        double totalDistance = dist[end][cityNode] + (dist[start][end] - signDist);
+                        validSigns.add(new Sign(cityName, Math.round(totalDistance)));
+                    }
+                }
+
+                // sort sign list based on distance then name
+                validSigns.sort(Comparator.comparing(Sign::getTotalDistance).thenComparing(Sign::getName));
+                
+                // print
+                for (Sign s : validSigns) {
+                    System.out.printf("%-19s %d%n", s.getName(), s.getTotalDistance());
+                }
+                
+                // for gradel - so the last line wont have a new line
+                if (index < signs.size()) {
+                    System.out.println();
+                }          
             }
         }
-        
-        // parse input
-        // create graph representation <- list of edges?
-        // objects for roads, cities 
-        // find shortest path <- floyds algorithm
-        // output
+
         sc.close();
     }
 }
--- src/main/java/com/mycompany/project5algo/Sign.java
+++ src/main/java/com/mycompany/project5algo/Sign.java
@@ -4,10 +4,25 @@ public class Sign {
     public int from;
     public int to;
     public double distance;
+    public String name;
+    public long totalDistance;
 
     public Sign(int f, int t, double d) {
         from = f;
         to = t;
         distance = d;
+    }
+
+    public Sign(String n, long d) {
+        name = n;
+        totalDistance = d;
+    }
+
+    public long getTotalDistance() {
+        return totalDistance;
+    }
+
+    public String getName() {
+        return name;
     }
 }
